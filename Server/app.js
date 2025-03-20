ce172d50d6a0854f8067b628bd17fcb389fed642
mirilevi2005 <@040levi@gmail.com>
2025-03-20 21:20:35 +0200

    connect mongoDB

--- Server/app.js
+++ Server/app.js
@@ -1,39 +1,53 @@
-const express=require('express');
-const mongoose=require('mongoose');
-const cors=require("cors")
 
-const app=express();
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const mongoose = require('mongoose');
+const cors = require("cors");
+require('dotenv').config();
+const connectDB = require('./config/dbConn');
+
+
+// חבר את מסד הנתונים לפני התחלת השרת
 connectDB();
+
 app.use(express.json());
 
+const corsOptions = require("./config/corsOptions");
 
-const corsOptions=require("./config/corsOptions")
-const connectDB=require('./config/dbConn')
+app.use(cors(corsOptions));
 
-const PORT=process.env.PORT||5000
-
-const LearningMaterials=require('./router/LearningMaterialsRouter');
-const Lecturer=require('./router/LearningMaterialsRouter');
-const Student=require('./router/LearningMaterialsRouter');
-
-app.use(cors(corsOptions))
-app.use('/NameOfLeucte/nameMatiral',LearningMaterials);
-app.use('/NameOfLeucte',Lecturer);
-app.use('/NameOfStudent',Student);
-
-app.listen(PORT,()=>{
-    console.log(`server is runing on port ${PORT}`); 
-})
+// ייבוא המודולים הנכונים
+const LearningMaterials = require('./router/LearningMaterialsRouter');
+//const Lecturer = require('./router/LecturerRouter');  // וודא שהשמות נכונים
+//const Student = require('./router/StudentRouter');  // וודא שהשמות נכונים
 
 
-mongoose.connection.once('open',()=>{
-    console.log('Connected')
-    app.listen(PORT,()=>{
-    console.log(`Server running in port ${PORT}`)
-})
-})
-mongoose.connection.on('error',err=>{
-    console.log(err)
-})
+
+
+// הגדרת הנתיבים (routes)
+app.use('/NameOfLecturer/nameMaterial', LearningMaterials);
+//app.use('/NameOfLecturer', Lecturer);
+//app.use('/NameOfStudent', Student);
+
+
+mongoose.connection.once('open', () => {
+  console.log('Connected to MongoDB ');
+  app.listen(PORT, () => { 
+    console.log(`Server running on port ${PORT}`);
+  });
+});
+
+// טיפול בשגיאות חיבור למונגו
+mongoose.connection.on('error', err => {
+  console.error('Error connecting to MongoDB:', err);
+});
+
+
+
+
+
+
 
 
